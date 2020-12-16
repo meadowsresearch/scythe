be1635e0ace50ed4a83ae84a454039995f9823ed
Jasper van den Bosch <japsai@gmail.com>
2020-12-16 15:58:44 +0100

    notes on the interface for exporting dfm/gabor data

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # scythe
 Scythe: the Meadows Export & Analysis toolkit
+
+- RSA on one-, two- and n-dimensional rating data
+- Diagnostic Feature Mapping features
---
+++ dfm_export_example.py
@@ -0,0 +1,16 @@
+## in: path to json file for single participant, single task
+## intermediate: trials*featureIndices, stimuli*features
+## out: trials*features
+
+## fully array based:
+"""
+## fully array based:
+
+trials x (max) features, dtype= boolean
+trials x meta (object, response, etc), dataframe
+objects x features x params (freq, ori, x, y)
+objects x gabvects
+
+
+PARAMS (6): size, freq, oris, phase, x, y;
+"""
