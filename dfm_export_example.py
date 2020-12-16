bb675a0b51fc92039c4f0b44f185fa79763879f8
Jasper van den Bosch <japsai@gmail.com>
2020-12-16 18:12:41 +0100

    unpacking feat ids

--- dfm_export_example.py
+++ dfm_export_example.py
@@ -12,10 +12,12 @@ PARAMS (6): size, freq, oris, phase, x, y;
 from os.path import join, expanduser
 import json
 import pandas
+from meadows.rng import RandomNumberGenerator
 
 data_dir = expanduser('~/Data/scythe/dfm')
 fname = 'Meadows_GaborsTest_v_v1_meet-wombat_1_annotations.csv'
 fpath = join(data_dir, fname)
+rng = RandomNumberGenerator()
 
 ## read annotations and embellish
 df = pandas.read_csv(fpath)
@@ -29,14 +31,30 @@ df = df.drop(['stim2_id', 'stim2_name','stim3_id', 'stim3_name'], axis=1)
 
 ## read features
 stim_dfs = dict()
+size = -1
 for sid in stim_ids:
     fpath = join(data_dir, f'{sid}.gabors')
     with open(fpath) as fhandle:
         data = json.load(fhandle)
     name = data['source']['name']
+    size = data['settings']['size']
     freqs = data['settings']['frequencies']
     oris = data['settings']['orientations']
     stim_df = pandas.DataFrame(data['features'])
     stim_df['frequency'] = [freqs[f] for f in stim_df.f]
     stim_df['orientation'] = [oris[o] for o in stim_df.o]
     stim_dfs[name] = stim_df
+
+## from json can get size, from annotations have to load features
+feat_idx = []
+for t, trial in df.iterrows():
+    nfeats_max = len(stim_dfs[trial.stim1_name].index)
+    feat_idx.append(rng.sample_indices(
+        pop_size=nfeats_max,
+        n=trial.nfeats,
+        seed=trial.seed
+    ))
+df['feat_idx'] = feat_idx
+
+## bool array
+max_feats = max([len(sdf.index) for sdf in stim_dfs.values()])
