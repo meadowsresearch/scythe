bd890468466b1a09955391f6b39974bdbc21c7d5
Jasper van den Bosch <japsai@gmail.com>
2021-09-01 11:18:02 +0100

    key for cfmt responses

---
+++ tests/cfmt_score.py
@@ -0,0 +1,17 @@
+"""Unit tests for 
+"""
+#pylint: disable=import-outside-toplevel, no-self-use
+from unittest import TestCase
+
+
+class CfmtScoreTests(TestCase):
+    """Unit tests for 
+    """
+
+    def test_align(self):
+        """The align method bring the RDMs as close together as possible
+        """
+        from meadows.cfmt import score_cfmt
+        ## make a fake annotations df
+        ## then expect the return to have a correct column
+        self.fail()
