f903f368871523798e4a32cfc1cf06ef96ddb046
Jasper van den Bosch <japsai@gmail.com>
2022-02-05 23:17:19 +0000

    implemented json -> DataFrame for MA

--- meadows/io/pandas.py
+++ meadows/io/pandas.py
@@ -1,10 +1,39 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING, Tuple
 import json
 from pandas import DataFrame
+from rsatoolbox.io.meadows import extract_filename_segments
 
 
 def load_dataframe(fpath: str) -> DataFrame:
+    """Read data from json file and return as pandas DataFrame
+
+    Currently only supports single task, single participant MA 
+
+    Args:
+        fpath (str): full path to json file
+
+    Returns:
+        DataFrame: long-form DataFrame
+    """
+    info = extract_filename_segments(fpath)
     with open(fpath) as fhandle:
         data = json.load(fhandle)
-    return DataFrame([])
+    stimuli = dict([(s['id'], s['name']) for s in data['stimuli']])
+    rows = []
+    for t, trial in enumerate(data['trials']):
+        for position in trial['positions']:
+            rows.append(dict(
+                stim_id=position['id'],
+                stim_fname=stimuli[position['id']],
+                x=position['x'],
+                y=position['y'],
+                trial=t,
+                trial_start=trial['start'],
+                trial_end=trial['end'],
+            ))
+    df = DataFrame(rows)
+    df['task_name'] = data['task']['name']
+    df['participant'] = info['participant']
+    df['task_index'] = info['task_index']
+    df['experiment_name'] = info['experiment_name']
+    return df
--- tests/io_pandas.py
+++ tests/io_pandas.py
@@ -18,24 +18,25 @@ class IoPandasTests(TestCase):
         fname = 'Meadows_myExp_v_v1_cuddly-bunny_3_tree.json'
         fpath = pkg_resources.resource_filename('tests', 'data/' + fname)
         df = load_dataframe(fpath)
-        self.assertEqual(df['trial'].unique.size, 109)
-        NROWS = 999
-        self.assertEqual(len(df), NROWS)
+        self.assertEqual(df['trial'].unique().size, 109)
+        N_PLACEMENTS_TOTAL = TASK_ROWS = 1758
+        self.assertEqual(len(df), N_PLACEMENTS_TOTAL)
         ## file-level descriptors
-        def random_row():
-            return randint(0, NROWS-1)
-        self.assertEqual(df['participant'][random_row()], 'cuddly-bunny')
-        self.assertEqual(df['task_index'][random_row()], 3)
-        self.assertEqual(df['task_name'][random_row()], 'arrangement')
-        self.assertEqual(df['experiment_name'][random_row()], 'myExp')
+        def random_row(n_rows):
+            return randint(0, n_rows-1)
+        self.assertEqual(df['participant'][random_row(TASK_ROWS)], 'cuddly-bunny')
+        self.assertEqual(df['task_index'][random_row(TASK_ROWS)], 3)
+        self.assertEqual(df['task_name'][random_row(TASK_ROWS)], 'arrangement')
+        self.assertEqual(df['experiment_name'][random_row(TASK_ROWS)], 'myExp')
         ## trial-level descriptors
-        # trial index
+        TRIAL = 23
+        TRIAL_ROWS = 4
+        trial_df = df[df['trial'] == TRIAL].reset_index()
+        self.assertEqual(len(trial_df), TRIAL_ROWS)
+        self.assertEqual(trial_df['trial_start'][random_row(TRIAL_ROWS)], 1509300051496)
+        self.assertEqual(trial_df['trial_end'][random_row(TRIAL_ROWS)], 1509300064445)
         ## stimulus-level descriptors
-        # stim id
-        # stim name
-        # x
-        # y
-        assert_array_equal(df['stim_name'], ['stim118', 'stim117'])
-        assert_array_equal(df['stim_id'], ['stim118', 'stim117'])
-        assert_array_almost_equal(df['x'],
-            [0.00791285387561264, 0.00817090931233484])
+        self.assertEqual(trial_df['stim_id'][0], '59f240584eae357c0f039786')
+        self.assertEqual(trial_df['x'][0], 1.3041179991844583)
+        self.assertEqual(trial_df['y'][0], 0.8458686782494096)
+        self.assertEqual(trial_df['stim_fname'][0], 'stim069.png')
