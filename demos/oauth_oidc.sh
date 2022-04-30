134c4f91d7ee0af25565c3da24ddcf467bb6bf54
Jasper van den Bosch <japsai@gmail.com>
2022-04-30 03:41:12 +0100

    progress on API shell script

--- demos/oauth_oidc.sh
+++ demos/oauth_oidc.sh
@@ -2,13 +2,35 @@
 ## with OAuth2 / OpenID connect to get access
 ## to your own user data
 
+##TODO: replace domain with https://meadows-research.com
+
 ## open a browser and navigate to the authorization page
-xdg-open meadows-research.com/authorize
+xdg-open "https://staging.meadows-research.com/oauth2/authorize?\
+client_id=your.app.foo&\
+redirect_uri=https://httpbin.org/get&\
+state=xyz&\
+scope=test"
 
-## 2. this should trigger a GET to the client,
-##    have to use external service to gather,
-## 3. exchange auth token for access + refresh
-curl meadows-research.com/authorize
+## After submitting the form with the correct login details,
+## you should see the httpbin page displaying the plain request data,
+## starting with something like:
+# {
+#   "args": {
+#     "code": "abc.def", 
+#     "state": "xyz"
+#   }, 
 
-## 4. access protected page with access token
-curl meadows-research.com/users/jdoe
+## 2. exchange auth token for access + refresh
+curl -X POST \
+-d 'grant_type=authorization_code' \
+-d 'code=abc.def' \
+-d 'client_id=your.app.foo' \
+-d 'client_secret=YourSecret' \
+https://staging.meadows-research.com/oauth2/token
+
+## This will print the json response, which should look something like:
+# {"token_type": "Bearer", "access_token": "ghi.jkl", "expires_in": 86400.0}
+
+## 3. access protected page with access token
+curl -H 'Authorization: Bearer ghi.jkl' https://staging.meadows-research.com/users/jdoe
+
