98dbddd31d3c8e463d9bb1b2be47431dcd90cca9
Jasper van den Bosch <japsai@gmail.com>
2022-04-30 17:43:48 +0100

    finished demo script oauth2

--- demos/oauth_oidc.sh
+++ demos/oauth_oidc.sh
@@ -2,10 +2,16 @@
 ## with OAuth2 / OpenID connect to get access
 ## to your own user data
 
-##TODO: replace domain with https://meadows-research.com
+## Client ID: "your.app.foo" You receive this from us.
+## Client Secret: "YourSecret" You receive this from us.
+## Redirect URI: URL of your app to for a GET callback with the 
+##     Authorization Code. In the example we're using httpbin for simplicity.
+## State: Optional. This will be forwarded back to your app.
+## Scope: Optional. Not in use right now.
 
+## Step 1. Ask end-user to authorize your app
 ## open a browser and navigate to the authorization page
-xdg-open "https://staging.meadows-research.com/oauth2/authorize?\
+xdg-open "https://meadows-research.com/oauth2/authorize?\
 client_id=your.app.foo&\
 redirect_uri=https://httpbin.org/get&\
 state=xyz&\
@@ -20,17 +26,16 @@ scope=test"
 #     "state": "xyz"
 #   }, 
 
-## 2. exchange auth token for access + refresh
+## Step 2. exchange Authorization Code for Access Token
 curl -X POST \
 -d 'grant_type=authorization_code' \
 -d 'code=abc.def' \
 -d 'client_id=your.app.foo' \
 -d 'client_secret=YourSecret' \
-https://staging.meadows-research.com/oauth2/token
+https://meadows-research.com/oauth2/token
 
 ## This will print the json response, which should look something like:
 # {"token_type": "Bearer", "access_token": "ghi.jkl", "expires_in": 86400.0}
 
-## 3. access protected page with access token
-curl -H 'Authorization: Bearer ghi.jkl' https://staging.meadows-research.com/users/jdoe
-
+## 3. Access protected page with access token
+curl -i -H 'Accept: application/json' -H 'Authorization: Bearer ghi.jkl' https://meadows-research.com/users/jdoe
